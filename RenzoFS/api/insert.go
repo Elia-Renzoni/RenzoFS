4fde0a6a9bdc0025dd6b334cab8ec2c85c0c3351
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-03-03 10:37:04 +0100

    Add files via upload

--- RenzoFS/api/insert.go
+++ RenzoFS/api/insert.go
@@ -15,6 +15,7 @@ import (
 )
 
 type InsertPayLoad struct {
+	QueryType    string   `json:"query_type"`
 	User         string   `json:"user"`
 	FileName     string   `json:"file_name"`
 	QueryContent []string `json:"query_content"`
