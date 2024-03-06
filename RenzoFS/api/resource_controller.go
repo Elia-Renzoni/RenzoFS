c7d65ae00ca63efe9b4d6f9099295d896ae7fee7
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-03-06 15:18:47 +0100

    Update resource_controller.go

--- RenzoFS/api/resource_controller.go
+++ RenzoFS/api/resource_controller.go
@@ -15,33 +15,6 @@ import (
 type ResourceController struct {
 }
 
-/*func (r *ResourceController) checkDir(name string) (result bool) {
-	if dirInfo, err := os.Stat(name); dirInfo.IsDir() {
-		result = true
-	} else if dirInfo != nil {
-		fmt.Printf("Qualcosa di strano è successa")
-	} else if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Printf("La Directory non esiste")
-			result = false
-		}
-		log.Fatal(err)
-	}
-	return
-}
-
-func (r *ResourceController) checkFile(name string) (result bool) {
-	if fileInfo, err := os.Stat(name); fileInfo.Name() != name { // to test
-		result = true
-	} else if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Printf("Ïl File non esiste")
-			result = false
-		}
-	}
-	return
-}*/
-
 func (r *ResourceController) createNewDir(dirname string) {
 	if err := os.MkdirAll("renzofs_local_file_system/"+dirname, os.ModeDir); err != nil {
 		log.Fatal(err)
