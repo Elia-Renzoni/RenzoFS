04ecee0719b68cef3efd6468493b644a926bc6b4
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-03-18 13:36:17 +0100

    Update delete_dir.go

--- RenzoFS/api/delete_dir.go
+++ RenzoFS/api/delete_dir.go
@@ -3,6 +3,7 @@ package api
 import "net/http"
 
 type DeleteDirPayLoad struct {
+  dirToDeleteName string
 }
 
 func (d *DeleteDirPayLoad) HandleDirElimination(w http.ResponseWriter, r *http.Request) {
