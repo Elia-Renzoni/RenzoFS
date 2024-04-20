f66ceec6a9adfaaaab7f679e6e7313b6cdce58af
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-04-20 15:54:59 +0200

    Update reverse_proxy_server.go

--- RenzoFS/reverse_proxy_server.go
+++ RenzoFS/reverse_proxy_server.go
@@ -14,23 +14,5 @@ import (
 )
 
 func main() {
-	buffer := make([]byte, 1024)
-	listener, err := net.Listen("tcp", ":3030")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer listener.Close()
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if _, err := conn.Read(buffer); err != nil || err == io.EOF {
-			break
-		}
-		fmt.Printf("%s", string(buffer))
-		conn.Close()
-	}
+	// TODO
 }
