f5f5004e5a5eb174cb652826bdde6d4b42742193
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-05-22 14:42:03 +0200

    Add files via upload

--- RenzoFS/authentication_service/server.go
+++ RenzoFS/authentication_service/server.go
@@ -1,1 +1,12 @@
 package main
+
+import "net/http"
+
+func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/login")
+	router.HandleFunc("/logout")
+
+	http.ListenAndServe(":8082", router)
+}
