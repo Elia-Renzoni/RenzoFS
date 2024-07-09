5854ebf160780fd7335bd29a4fd9bf1397747081
unknown <elia.renzoni03@gmail.com>
2024-07-09 15:44:30 +0200

    delete login.go and add servers to server pool

--- RenzoFS/api_gateway.go
+++ RenzoFS/api_gateway.go
@@ -13,14 +13,16 @@ import (
 func main() {
 	apiGateway := renzofsapigateway.NewRenzoFSAPIGateway("localhost", ":4040")
 	apiGateway.AddMicroservice("read", "http://127.0.0.1:8080",
-								"insert", "http://127.0.0.1:8080",
-								"update", "http://127.0.0.1:8080",
-								"delete", "http://127.0.0.1:8080",
-								"deletedir", "http://127.0.0.1:8080",
-								"createdir", "http://127.0.0.1:8080",
-								"fileinfo", "http://127.0.0.1:8080",
-								"statistics", "http://127.0.0.1:8081",
-								"login", "http://127.0.0.1:8082",
-								"logout", "http://127.0.0.1:8082")
+		"insert", "http://127.0.0.1:8080",
+		"update", "http://127.0.0.1:8080",
+		"delete", "http://127.0.0.1:8080",
+		"deletedir", "http://127.0.0.1:8080",
+		"createdir", "http://127.0.0.1:8080",
+		"fileinfo", "http://127.0.0.1:8080",
+		"statistics", "http://127.0.0.1:8081",
+		"login", "http://127.0.0.1:8082",
+		"logout", "http://127.0.0.1:8082",
+		"registry", "http://127.0.0.1:8082",
+		"deregistry", "http://127.0.0.1:8082")
 	apiGateway.StartListeningRequests()
 }
