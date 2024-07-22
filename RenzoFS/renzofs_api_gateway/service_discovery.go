1e86d7b5e51dd3a641b9e3d189555aaf387af34a
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-07-22 17:43:30 +0200

    add service discovery

--- RenzoFS/renzofs_api_gateway/service_discovery.go
+++ RenzoFS/renzofs_api_gateway/service_discovery.go
@@ -1,30 +1,47 @@
 package renzofsapigateway
 
-// il servizio deve periodicamente mandare una richiesta
-// ai server che si trovano nel server pool.
-// Legge il server pool e prende la porta e l'host,
-// poi invia a quell'host una richeista all'endpoint
-// health. Il server in questione deve rispondere
-// con lo stato del sistema, In risposta i server
-// devono mandare lo stato della connessione
-// come risposta. Se i server non inviano in tempo
-// lo stato (da notare ogni richiesta ha un tempo di
-// massimo 5 secondi), allora singifica che il server è in down
-// e quindi va eliminato dal server pool.
+import (
+	"net/http"
+	"sync"
+	"time"
+)
 
 type ServiceDiscovery struct {
+	services []string
 	RenzoFSAPIGateway
-	toDelete <-chan string
+	mutex sync.Mutex
 }
 
-func (s *ServiceDiscovery) ManageServerPool() {
-
+func NewServiceDiscovery() ServiceDiscovery {
+	return ServiceDiscovery{}
 }
 
-func visitServerPool(pool map[string]string) bool {
-	return false
+func (s *ServiceDiscovery) takeValuesAndTrim() {
+	var (
+		i    int
+		oldV string
+	)
+
+	for _, value := range s.serverPool {
+		if i != 0 {
+			if oldV != value {
+				s.services = append(s.services, value)
+			}
+		}
+		oldV = value
+		i++
+	}
 }
 
-func deleteServerFromPool(keys ...string) {
-
+func (s *ServiceDiscovery) Broadcast() {
+	for _, value := range s.services {
+		go func(server string) {
+			var decodedRes string
+			client := &http.Client{
+				Timeout: 3 * time.Second,
+			}
+			response, err := client.Get(server)
+			// TODO
+		}(value)
+	}
 }
